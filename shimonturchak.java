47861491f54a698aaa8e2fcace78065191922ee2
SimonTurchak <107707572+SimonTurchak@users.noreply.github.com>
2023-07-19 13:55:37 +0300

    Update shimonturchak.java

--- shimonturchak.java
+++ shimonturchak.java
@@ -1,7 +1,7 @@
 public class shimonturchak{
 	public static void main(String[] args) {
 		for (int i=1;i<=10; i++) {
-			System.out.println("Shimon Turchakk"+i);
+			System.out.println("Shimon Turchakkk"+i);
 		}
 	}
 }
